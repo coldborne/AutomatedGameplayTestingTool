15bf0a2178ec7119e2f0a1c42297d09e936d8539
coldborne <coldborneVV@gmail.com>
2026-04-26 19:18:46 +0300

    Реализован класс для удобного сбора информации о проденных этапах и игре в целом

---
+++ Assets/Scripts/AutoTests/GameplayTestReport.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AutoTests
+{
+    [Serializable]
+    public class GameplayTestReport
+    {
+        public string TestName;
+        public bool IsPassed;
+        public float AverageFps;
+        public bool HasBotReachedFinish;
+        public bool IsBotStuck;
+        public bool IsButtonPressed;
+        public bool IsDoorOpened;
+        public bool IsCapturePointCompleted;
+        public string FailureReason;
+    }
+}
